103790065156901eed34206485b2c105d7c5537e
erge <roman.grabovskiy@hotmail.com>
2022-08-24 18:35:06 +0300

    add tag menu

--- blog/models.py
+++ blog/models.py
@@ -1,31 +1,39 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Category(models.Model):
     title = models.CharField(max_length=255)
-    slag = models.SlugField(max_length=255, verbose_name="URL", unique=True)
+    slug = models.SlugField(max_length=255, verbose_name="URL", unique=True)
 
     def __str__(self):
         return self.title
 
+    def get_absolute_url(self):
+        return reverse("category", kwargs={"slug": self.slug})
+
     class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
         ordering = ["title"]
 
 
 class Tag(models.Model):
     title = models.CharField(max_length=50)
-    slag = models.SlugField(max_length=50, verbose_name="URL", unique=True)
+    slug = models.SlugField(max_length=50, verbose_name="URL", unique=True)
 
     def __str__(self):
         return self.title
 
     class Meta:
+        verbose_name = "Tag"
+        verbose_name_plural = "Tags"
         ordering = ["title"]
 
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
-    slag = models.SlugField(max_length=255, verbose_name="URL", unique=True)
+    slug = models.SlugField(max_length=255, verbose_name="URL", unique=True)
     author = models.CharField(max_length=100)
     content = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Published")
@@ -38,4 +46,6 @@ class Post(models.Model):
         return self.title
 
     class Meta:
+        verbose_name = "Post"
+        verbose_name_plural = "Posts"
         ordering = ["-created_at"]
